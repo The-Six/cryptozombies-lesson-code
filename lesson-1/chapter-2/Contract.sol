fbfbde5f8dcdb16485eeaa32fcf4a7fcb4dd4508
The-Six <ijazdriscoll@gmail.com>
2022-04-06 23:44:19 +0100

    chapter 2 notes

--- lesson-1/chapter-2/Contract.sol
+++ lesson-1/chapter-2/Contract.sol
@@ -1,5 +1,5 @@
 
-pragma solidity ^0.4.25;
+pragma solidity ^0.4.25; //this represents the solidity version, this has to be declared before every smart.contract
 
       contract ZombieFactory {
 
--- lesson-1/chapter-3/Contract.sol
+++ lesson-1/chapter-3/Contract.sol
@@ -2,6 +2,6 @@ pragma solidity ^0.4.25;
 
 contract ZombieFactory {
 
-    uint dnaDigits = 16;
+    uint dnaDigits = 16; //uint stands for unsigned integer, meaning its value must be non-negative. There's also an int data type for signed integers.
 
 }
