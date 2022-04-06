b7a4b75b9dc8cca80bef2cff946d8e7e85ee0c18
The-Six <ijazdriscoll@gmail.com>
2022-04-06 23:51:38 +0100

    notes on chapter 5, lesson 1 - structs

--- lesson-1/chapter-5/Contract.sol
+++ lesson-1/chapter-5/Contract.sol
@@ -5,7 +5,7 @@ contract ZombieFactory {
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
-    struct Zombie {
+    struct Zombie { // this is for when I need more complex data types (kinda like objects in my mind - data types with multiple properties)
         string name;
         uint dna;
     }
